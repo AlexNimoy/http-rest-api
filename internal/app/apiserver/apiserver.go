90e4181019c0520405cfb0a9d3a1681208d5cff7
Александр Поляков <backstabe@gmail.com>
2019-12-17 00:59:17 +0300

    feature: hello route

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,23 @@
 package apiserver
 
 import (
+  "io"
+	"net/http"
 	"github.com/sirupsen/logrus"
+  "github.com/gorilla/mux"
 )
 
 type APIServer struct{
   config *Config
   logger *logrus.Logger 
+  router *mux.Router
 }
 
 func New(config *Config) *APIServer {
   return &APIServer{
     config: config,
     logger: logrus.New(),
+    router: mux.NewRouter(),
   }
 }
 
@@ -21,9 +26,10 @@ func (s *APIServer) Start() error {
     return err
   }
 
+  s.configureRouter()
   s.logger.Info("starting API server")
 
-  return nil
+  return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -35,4 +41,14 @@ func (s *APIServer) configureLogger() error {
   s.logger.SetLevel(level)
 
   return nil
+}
+
+func (s *APIServer) configureRouter() {
+  s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "Hello")
+  }
 }
