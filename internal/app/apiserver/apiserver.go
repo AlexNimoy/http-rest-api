db1aa76cedd9093a4f862ecc4b64970bed9fe592
Александр Поляков <backstabe@gmail.com>
2019-12-26 19:48:58 +0300

    postgres connection

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"github.com/sirupsen/logrus"
   "github.com/gorilla/mux"
+  "github.com/Backstabe/http-rest-api/internal/app/store"
 )
 
 type APIServer struct{
   config *Config
   logger *logrus.Logger 
   router *mux.Router
+  store  *store.Store
 }
 
 func New(config *Config) *APIServer {
@@ -27,6 +29,11 @@ func (s *APIServer) Start() error {
   }
 
   s.configureRouter()
+
+  if err := s.configureStore(); err != nil {
+    return err
+  }
+
   s.logger.Info("starting API server")
 
   return http.ListenAndServe(s.config.BindAddr, s.router)
@@ -45,6 +52,18 @@ func (s *APIServer) configureLogger() error {
 
 func (s *APIServer) configureRouter() {
   s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStore() error {
+  st := store.New(s.config.Store)
+
+  if err := st.Open(); err != nil {
+    return err
+  }
+
+  s.store = st
+
+  return nil
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
