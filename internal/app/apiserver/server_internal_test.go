66eaf54e8264b6c2b2cf171b7c77ea5eb795a03a
Александр Поляков <backstabe@gmail.com>
2020-02-02 01:14:40 +0300

    users correct test

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -5,15 +5,40 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"bytes"
+	"encoding/json"
 	"github.com/Backstabe/http-rest-api/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestServer_HandleUsersCreate(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
 	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
 
-	assert.Equal(t, rec.Code, http.StatusOK)
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    "user@examle.com",
+				"password": "password",
+			},
+			expectedCode: http.StatusCreated,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/users", b)
+			s.ServeHTTP(rec, req)
+
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+
 }
