62572ec356aa2dc40356ccee6df899afaed055bc
Александр Поляков <backstabe@gmail.com>
2020-02-02 02:45:36 +0300

    check user password

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -2,11 +2,16 @@ package apiserver
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/Backstabe/http-rest-api/internal/app/model"
 	"github.com/Backstabe/http-rest-api/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"net/http"
+)
+
+var (
+	errIncorrectEmailOrPassword = errors.New("incorrect email or password")
 )
 
 type server struct {
@@ -33,6 +38,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (s *server) configureRouter() {
 	s.router.HandleFunc("/users", s.handleUsersCreate()).Methods("POST")
+	s.router.HandleFunc("/sessions", s.handleSessionsCreate()).Methods("POST")
 }
 
 func (s *server) handleUsersCreate() http.HandlerFunc {
@@ -73,5 +79,29 @@ func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data 
 	w.WriteHeader(code)
 	if data != nil {
 		json.NewEncoder(w).Encode(data)
+	}
+}
+
+func (s *server) handleSessionsCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u, err := s.store.User().FindByEmail(req.Email)
+		if err != nil || !u.ComparePassword(req.Password) {
+			s.error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, nil)
 	}
 }
