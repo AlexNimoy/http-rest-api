ee29656b9d3e03e1c5a5f69581a6660deb36aa83
Александр Поляков <backstabe@gmail.com>
2020-02-02 18:19:57 +0300

    header request id

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/go-ozzo/ozzo-validation/v3 v3.8.1
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/google/uuid v1.1.1
 	github.com/gopherschool/http-rest-api v0.0.0-20190922093049-d59926c5ce3c
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/Backstabe/http-rest-api/internal/app/model"
 	"github.com/Backstabe/http-rest-api/internal/app/store"
+	"github.com/google/uuid"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
@@ -14,8 +15,9 @@ import (
 )
 
 const (
-	sessionName        = "session-go-api"
-	ctxKeyUser  ctxKey = iota
+	sessionName            = "session-go-api"
+	ctxKeyUser      ctxKey = iota
+	ctxKeyRequestID ctxKey = iota
 )
 
 var (
@@ -50,6 +52,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) configureRouter() {
+	s.router.Use(s.setRequestID)
 	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
 	s.router.HandleFunc("/users", s.handleUsersCreate()).Methods("POST")
 	s.router.HandleFunc("/sessions", s.handleSessionsCreate()).Methods("POST")
@@ -58,6 +61,14 @@ func (s *server) configureRouter() {
 	private := s.router.PathPrefix("/private").Subrouter()
 	private.Use(s.authenticateUser)
 	private.HandleFunc("/whoami", s.handleWhoami()).Methods("GET")
+}
+
+func (s *server) setRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		w.Header().Set("X-Request-ID", id)
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyRequestID, id)))
+	})
 }
 
 func (s *server) authenticateUser(next http.Handler) http.Handler {
